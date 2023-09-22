1f2fe5b9b15fab3920b6df50d0df6dfffe7936f4
610engineer <67955620+610engineer@users.noreply.github.com>
2023-09-22 22:47:49 +0900

    issue#1

--- feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
+++ feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
@@ -1,20 +1,21 @@
 package com.oishikenko.android.recruitment.feature.list
 
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.consumedWindowInsets
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -28,10 +29,14 @@ fun RecipeListScreen(
     val cookingRecords by viewModel.cookingRecords.collectAsStateWithLifecycle()
     Scaffold(
         topBar = {
+            val imagePainter = painterResource(id = R.drawable.header)
             TopAppBar(
-                modifier = Modifier,
+                backgroundColor = Color.White
             ) {
-                Text(text = stringResource(id = R.string.cooking_records_title))
+                Image(painter = imagePainter,
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxWidth().height(100.dp)
+                )
             }
         }
     ) { innerPadding ->
